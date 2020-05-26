1cf9031594ffe731ad125cab7a26053fc239498c
cedashu <ashutoshverma@makewebbetter.com>
2020-05-27 04:55:27 +0530

    Mobile view replicated as shopify

--- public/js/youveda-checkout-public.js
+++ public/js/youveda-checkout-public.js
@@ -15,23 +15,38 @@ jQuery(document).ready(function($){
 
 		if ( 0 < jQuery( '.woocommerce-checkout-review-order-table' ).length ) {
 			jQuery( '.woocommerce-checkout-review-order-table' ).hide();
-			// jQuery( '.woocommerce-checkout-review-order-table' ).addClass( 'hidden_section' );
 		}
 		
-		// Show sections on click.
+		/** 
+		 * Show sections on click.
+		 * Login Section.
+		 */
 		jQuery(document).on( 'click', '.mwb_youveda_logged_in_toggle', function(e) {
 
 			login_section.toggleClass( 'hide_checkout_sections' );
-			jQuery( '#customer_details' ).toggle( 'slow' );
+			jQuery( '#customer_details' ).slideToggle( 'slow' );
 		});
 
+		/** 
+		 * Show sections on click.
+		 * Order Summary Section.
+		 */
 		jQuery(document).on( 'click', '.order_review_heading_toggle', function(e) {
 
 			jQuery( '.woocommerce-checkout-review-order-table' ).toggleClass( 'hidden_section' );
-			jQuery( '.woocommerce-checkout-review-order-table' ).toggle( 'slow' );
+			jQuery( '.woocommerce-checkout-review-order-table' ).slideToggle( 'slow' );
+
+			// Is visible.
+			if ( jQuery( ".woocommerce-checkout-review-order-table" ).hasClass( 'hidden_section' ) ) { 
+			  jQuery( '.order_review_text' ).text( 'Hide Order Summary' );
+			} 
+			// Is Hidden.
+			else {
+			  jQuery( '.order_review_text' ).text( 'Show Order Summary' );
+			}
 		});
 	
-	// End if Mbbile View.
+	// End if M0bile View.
 	}
 
 	// Hide Text from labels in payment methods.
--- public/templates/woocommerce/checkout/form-checkout.php
+++ public/templates/woocommerce/checkout/form-checkout.php
@@ -19,6 +19,31 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
+/* Add order review for mobile view here */
+if( function_exists( 'wp_is_mobile' ) && wp_is_mobile() ) : ?>
+
+	<div id="order_review" class="woocommerce-checkout-review-order">
+		<a class="order_review_heading_toggle" title="Click here to show order details." status="hidden" href="javascript:void(0);">
+			<h3 id="order_review_heading">
+				<span class="order_review_cart_icon">
+					<svg width="20" height="19" xmlns="http://www.w3.org/2000/svg" class="order-summary-toggle__icon">
+			            <path d="M17.178 13.088H5.453c-.454 0-.91-.364-.91-.818L3.727 1.818H0V0h4.544c.455 0 .91.364.91.818l.09 1.272h13.45c.274 0 .547.09.73.364.18.182.27.454.18.727l-1.817 9.18c-.09.455-.455.728-.91.728zM6.27 11.27h10.09l1.454-7.362H5.634l.637 7.362zm.092 7.715c1.004 0 1.818-.813 1.818-1.817s-.814-1.818-1.818-1.818-1.818.814-1.818 1.818.814 1.817 1.818 1.817zm9.18 0c1.004 0 1.817-.813 1.817-1.817s-.814-1.818-1.818-1.818-1.818.814-1.818 1.818.814 1.817 1.818 1.817z"></path>
+			         </svg>
+				</span>
+				<span class="order_review_text">
+					<?php esc_html_e( 'Show Order Summary', 'woocommerce' ); ?>
+				</span>
+				<span class="order_review_cart_total">
+					<?php echo function_exists( 'WC' ) && ! empty( WC()->cart ) ? WC()->cart->get_cart_total() :false; ?>
+				</span>
+			</h3>
+		</a>
+		<?php do_action( 'woocommerce_checkout_order_review' ); ?>
+		</div>
+	<?php do_action( 'woocommerce_checkout_after_order_review' );
+
+endif;
+
 do_action( 'woocommerce_before_checkout_form', $checkout );
 
 // If checkout registration is disabled and not logged in, the user cannot checkout.
@@ -57,21 +82,25 @@ if ( ! $checkout->is_registration_enabled() && $checkout->is_registration_requir
 	
 	<?php do_action( 'woocommerce_checkout_before_order_review' ); ?>
 
-	<div id="order_review" class="woocommerce-checkout-review-order">
-
-		<?php if ( function_exists( 'wp_is_mobile' ) && wp_is_mobile() ) : ?>
-			<a class="order_review_heading_toggle" href="javascript:void(0)">
-		<?php endif; ?>
-
-		<h3 id="order_review_heading" title="Click here to show order details."><?php esc_html_e( 'Order Summary', 'woocommerce' ); ?></h3>
-
-		<?php if ( function_exists( 'wp_is_mobile' ) && wp_is_mobile() ) : ?>
-			</a>
-		<?php endif; ?>
-		<?php do_action( 'woocommerce_checkout_order_review' ); ?>
-	</div>
-
-	<?php do_action( 'woocommerce_checkout_after_order_review' ); ?>
+	<?php if ( ! function_exists( 'wp_is_mobile' ) || ! wp_is_mobile() ) : ?>
+		<div id="order_review" class="woocommerce-checkout-review-order">
+			<h3 id="order_review_heading">
+				<span class="order_review_cart_icon">
+					<svg width="20" height="19" xmlns="http://www.w3.org/2000/svg" class="order-summary-toggle__icon">
+			            <path d="M17.178 13.088H5.453c-.454 0-.91-.364-.91-.818L3.727 1.818H0V0h4.544c.455 0 .91.364.91.818l.09 1.272h13.45c.274 0 .547.09.73.364.18.182.27.454.18.727l-1.817 9.18c-.09.455-.455.728-.91.728zM6.27 11.27h10.09l1.454-7.362H5.634l.637 7.362zm.092 7.715c1.004 0 1.818-.813 1.818-1.817s-.814-1.818-1.818-1.818-1.818.814-1.818 1.818.814 1.817 1.818 1.817zm9.18 0c1.004 0 1.817-.813 1.817-1.817s-.814-1.818-1.818-1.818-1.818.814-1.818 1.818.814 1.817 1.818 1.817z"></path>
+			         </svg>
+				</span>
+				<span class="order_review_text">
+					<?php esc_html_e( 'Order Summary', 'woocommerce' ); ?>
+				</span>
+				<span class="order_review_cart_total">
+					<?php echo function_exists( 'WC' ) && ! empty( WC()->cart ) ? WC()->cart->get_cart_total() :false; ?>
+				</span>
+			</h3>
+			<?php do_action( 'woocommerce_checkout_order_review' ); ?>
+		</div>
+		<?php do_action( 'woocommerce_checkout_after_order_review' ); ?>
+	<?php endif; ?>
 
 	<div class="form-row place-order mwb_youveda_place_order">
 		<noscript>
